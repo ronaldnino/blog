2b8380ff9fc8889ede9f1e331069c74d93499f37
Ronald Nino <ronaldnino@12.0.16.172.net-uno.net>
2023-10-04 18:26:17 -0400

    remove file

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/ronaldnino/Code/JavaScript/Docusaurus/my-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/ronaldnino/Code/JavaScript/Docusaurus/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/ronaldnino/Code/JavaScript/Docusaurus/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/ronaldnino/Code/JavaScript/Docusaurus/my-website/src/css/custom.css'),
-];
