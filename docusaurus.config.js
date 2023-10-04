9b11e7b5040341dfd0ea97e253192b2b5acb4264
Ronald Nino <ronaldnino@12.0.16.172.net-uno.net>
2023-10-04 18:55:39 -0400

    update CI/CD

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ronaldnino.github.io/blog/',
+  url: 'https://ronaldnino.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://ronaldnino.github.io/blog/',
+  baseUrl: '/blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
